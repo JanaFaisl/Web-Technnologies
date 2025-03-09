023e5f1a40055cf880580f0ad23f3730d291b464
JanaFaisl <jaanaa.12j@gmail.com>
2025-03-09 13:04:45 +0300

    lab 6

--- apps/bookmodule/urls.py
+++ apps/bookmodule/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('html5/text/formatting/', views.text_formatting_view, name='formatting_page'),
     path('html5/listing/', views.listing_page, name='listing_page'),
     path('html5/tables/', views.tables_page, name='tables_page'),
+    path('search/', views.search_books, name='search_books'),
 ]
